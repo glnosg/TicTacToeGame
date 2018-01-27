a3417b0db2399cbf088001f1464ed7efd09151cc
glnosg <glina.osg@gmail.com>
2018-01-27 19:20:49 +0100

    Improve custom game settings

--- app/src/main/java/com/example/android/tictacgrid/CustomMultiplayerSettingsActivity.java
+++ app/src/main/java/com/example/android/tictacgrid/CustomMultiplayerSettingsActivity.java
@@ -3,15 +3,18 @@ package com.example.android.tictacgrid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-import static com.example.android.tictacgrid.StandardMultiplayerSettingsActivity.STANDARD_GAME_GRID_DIMENSIONS;
 
 /**
  * Created by pawel on 27.01.18.
@@ -21,16 +24,32 @@ public class CustomMultiplayerSettingsActivity extends AppCompatActivity {
 
     static final int[] DEFAULT_GAME_GRID_DIMENSIONS = {15, 20};
     static final int DEFAULT_WINNING_CONDITION = 5;
-    ArrayList<String> listOfNames;
+    int numberOfPlayers = 2;
+    Toast mToast;
 
     ArrayList<EditText> listOfNamesEditTextFields;
+    ArrayList<EditText> listOfGridDimensionsEditTexts;
+    EditText winningConditionEditText;
     Spinner numberOfPlayersSpinner;
     Button startGameButton;
+    LinearLayout settingsForPlayer3, settingsForPlayer4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_multiplayer_settings);
+
+        setLayoutElements();
+    }
+
+    private void setLayoutElements() {
+
+        setEditTexts();
+        setSpinner();
+        setStartGameButton();
+    }
+
+    private void setEditTexts() {
 
         listOfNamesEditTextFields = new ArrayList<>();
         listOfNamesEditTextFields.add((EditText) findViewById(R.id.et_player1_name_custom_game));
@@ -38,18 +57,48 @@ public class CustomMultiplayerSettingsActivity extends AppCompatActivity {
         listOfNamesEditTextFields.add((EditText) findViewById(R.id.et_player3_name_custom_game));
         listOfNamesEditTextFields.add((EditText) findViewById(R.id.et_player4_name_custom_game));
 
-        numberOfPlayersSpinner = (Spinner) findViewById(R.id.spinner_how_many_players);
-        Integer[] spinnerItems = {1, 2, 3, 4};
-        ArrayAdapter<Integer> adapter =
-                new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems);
-        numberOfPlayersSpinner.setAdapter(adapter);
+        listOfGridDimensionsEditTexts = new ArrayList<>();
+        listOfGridDimensionsEditTexts.add((EditText) findViewById(R.id.et_number_of_grid_columns));
+        listOfGridDimensionsEditTexts.add((EditText) findViewById(R.id.et_number_of_grid_rows));
 
-        setButtons();
+        winningConditionEditText = (EditText) findViewById(R.id.et_how_many_in_line_to_win);
+
+        settingsForPlayer3 = (LinearLayout) findViewById(R.id.ll_player3_settings);
+        settingsForPlayer4 = (LinearLayout) findViewById(R.id.ll_player4_settings);
     }
 
-    private void setButtons() {
+    private void setSpinner() {
 
-        setStartGameButton();
+        numberOfPlayersSpinner = (Spinner) findViewById(R.id.spinner_how_many_players);
+        Integer[] spinnerItems = {2, 3, 4};
+        ArrayAdapter<Integer> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, spinnerItems);
+        numberOfPlayersSpinner.setAdapter(adapter);
+
+        numberOfPlayersSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                numberOfPlayers = (int) parent.getSelectedItem();
+
+                switch (numberOfPlayers) {
+                    case 2:
+                        settingsForPlayer3.setVisibility(View.GONE);
+                        settingsForPlayer4.setVisibility(View.GONE);
+                        break;
+                    case 3:
+                        settingsForPlayer3.setVisibility(View.VISIBLE);
+                        settingsForPlayer4.setVisibility(View.GONE);
+                        break;
+                    case 4:
+                        settingsForPlayer3.setVisibility(View.VISIBLE);
+                        settingsForPlayer4.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
 
     private void setStartGameButton() {
@@ -58,11 +107,11 @@ public class CustomMultiplayerSettingsActivity extends AppCompatActivity {
         startGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(getApplicationContext(), GameActivity.class);
 
-                listOfNames = new ArrayList<>();
-
-                for (int i = 0; i < listOfNamesEditTextFields.size(); ++i) {
+                ArrayList<String> listOfNames = new ArrayList<>();
+                for (int i = 0; i < numberOfPlayers; ++i) {
 
                     String name = listOfNamesEditTextFields.get(i).getText().toString();
                     if (name.equals("")) {
@@ -71,11 +120,47 @@ public class CustomMultiplayerSettingsActivity extends AppCompatActivity {
                     listOfNames.add(name);
                 }
 
+                int[] gridDimens = DEFAULT_GAME_GRID_DIMENSIONS;
+                String[] enteredDimens = {
+                        listOfGridDimensionsEditTexts.get(0).getText().toString(),
+                        listOfGridDimensionsEditTexts.get(1).getText().toString()};
+
+                for (int i = 0; i < enteredDimens.length; ++i) {
+
+                    Log.d("Custom", "" + enteredDimens[i]);
+
+                    if (!enteredDimens[i].equals("")) {
+                        try {
+                            gridDimens[i] = Integer.parseInt(enteredDimens[i]);
+                        } catch (NumberFormatException e) {
+                            showToast("Grid size: " + enteredDimens[i] + " is not a numerical value!");
+                        }
+                    }
+                }
+
+                int winningCondition = DEFAULT_WINNING_CONDITION;
+                String enteredWinningCondition = winningConditionEditText.getText().toString();
+
+                if (!enteredWinningCondition.equals("")) {
+                    try {
+                        winningCondition = Integer.parseInt(enteredWinningCondition);
+                    } catch (NumberFormatException e) {
+                        showToast("Winning condition: " + enteredWinningCondition + " is not a numerical value!");
+                    }
+                }
+
                 intent.putExtra("namesOfPlayers", listOfNames);
-                intent.putExtra("gridDimens", DEFAULT_GAME_GRID_DIMENSIONS);
-                intent.putExtra("howManyInLineToWin", DEFAULT_WINNING_CONDITION);
+                intent.putExtra("gridDimens", gridDimens);
+                intent.putExtra("howManyInLineToWin", winningCondition);
                 startActivity(intent);
             }
         });
+    }
+
+    private void showToast(String text) {
+        if (mToast != null)
+            mToast.cancel();
+        mToast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
+        mToast.show();
     }
 }
